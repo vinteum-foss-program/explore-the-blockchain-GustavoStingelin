d316374579790db8f4e06ed3c251a9df1eb14861
Gustavo Stingelin Cardoso Filho <gustavo.stingelin@outlook.com>
2024-11-24 17:49:55 -0300

    solve 003

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,8 @@
 # How many new outputs were created by block 123,456?
+BLOCK_NUMBER=123456
+BLOCK_HASH=$(bitcoin-cli getblockhash "$BLOCK_NUMBER")
+TOTAL_OUTPUTS=$(bitcoin-cli getblock "$BLOCK_HASH" | jq -r '.tx[]' | \
+    xargs -I {} bitcoin-cli getrawtransaction {} true | \
+    jq '[.vout | length] | add' | \
+    jq -s 'add')
+echo "$TOTAL_OUTPUTS"
