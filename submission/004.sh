64a7cf393291e8e2d2593b19b5826173d261befe
Gustavo Stingelin Cardoso Filho <gustavo.stingelin@outlook.com>
2024-11-24 18:56:23 -0300

    solve 004

--- submission/004.sh
+++ submission/004.sh
@@ -1,2 +1,10 @@
-# Using descriptors, compute the taproot address at index 100 derived from this extended public key:
+  # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
+XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
+INDEX="100"
+DESCRIPTOR="tr($XPUB/$INDEX)"
+CHECKSUM_DESCRIPTOR=$(bitcoin-cli getdescriptorinfo "$DESCRIPTOR" | jq -r '.descriptor')
+#echo "$CHECKSUM_DESCRIPTOR"
+ADDRESS=$(bitcoin-cli deriveaddresses "$CHECKSUM_DESCRIPTOR" | jq -r '.[0]')
+#echo "Taproot Address at index $INDEX: $ADDRESS"
+echo "$ADDRESS"
